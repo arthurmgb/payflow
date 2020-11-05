b26b8bad19731aa4935e88ed2b70b52eca82843c
Arthur Oliveira <arthurpmgb@gmail.com>
2020-11-05 20:20:07 -0300

    update nc 8

--- novo-contrato.php
+++ novo-contrato.php
@@ -9,7 +9,7 @@ $meses = filter_input(INPUT_POST, 'meses', FILTER_SANITIZE_NUMBER_INT);
 $observacoes = filter_input(INPUT_POST, 'observacoes', FILTER_SANITIZE_STRING);
 $cliente_Id = filter_input(INPUT_POST, 'cliente_id', FILTER_SANITIZE_NUMBER_INT);
 
-$inserir_contrato = "INSERT INTO contratos (created, vencimento, valor, meses, observacoes, id_servico, id_cliente) VALUES (NOW(), '$vencimento', '$valor', '$meses', '$observacoes', 6, '$cliente_Id')";
+$inserir_contrato = "INSERT INTO contratos (created, vencimento, valor, meses, observacoes, id_servico, id_cliente) VALUES (NOW(), '$vencimento', '$valor', '$meses', '$observacoes', 8, '$cliente_Id')";
 $executar_contrato = mysqli_query($conn, $inserir_contrato);
 
 if(mysqli_insert_id($conn)){
