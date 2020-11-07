766d9d0307ec9214e43b597e5a9505d7f023eca0
Arthur Oliveira <arthurpmgb@gmail.com>
2020-11-07 17:03:20 -0300

    Update mensalidades

--- novo-contrato.php
+++ novo-contrato.php
@@ -13,6 +13,14 @@ $servico_Id = filter_input(INPUT_POST, 'servico_id', FILTER_SANITIZE_NUMBER_INT)
 $inserir_contrato = "INSERT INTO contratos (created, vencimento, valor, meses, observacoes, id_servico, id_cliente) VALUES (NOW(), '$vencimento', '$valor', '$meses', '$observacoes', '$servico_Id', '$cliente_Id')";
 $executar_contrato = mysqli_query($conn, $inserir_contrato);
 
+$contrato_id = mysqli_insert_id($conn);
+
+for($i = 0; $i < $meses; $i++){
+    $inserir_mensalidades = "INSERT INTO mensalidades (id_cliente, id_servico, id_contrato, vencimento, pagamento, status, valor) VALUES ('$cliente_Id', '$servico_Id', '$contrato_id', '$vencimento', '0000-00-00', 'pendente', '$valor')";
+    $exec_mensal = mysqli_query($conn, $inserir_mensalidades);
+    $vencimento = date('Y-m-d', strtotime('+1 month', strtotime($vencimento)));
+}
+
 if(mysqli_insert_id($conn)){
     header("Location: contratos.php");
     $_SESSION['msg'] = "<div id='cliente_cad' class='alert alert-success' role='alert'>
