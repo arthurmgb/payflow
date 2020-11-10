9b851c1466fdce0b67294f5fc9ac99289e525ba9
marcosfreitas <marjrr18ptc@gmail.com>
2020-11-09 23:56:57 -0300

    paginação OK

--- contratos-search.php
+++ contratos-search.php
@@ -262,12 +262,10 @@
                   </thead>
                   <tbody>                  
                   <?php
-                  $pagina_atual = filter_input(INPUT_GET, 'pagina', FILTER_SANITIZE_NUMBER_INT);
-                  $pagina = (!empty($pagina_atual)) ? $pagina_atual : 1;
-                  $qtd_result = 1;
-                  $start = ($qtd_result * $pagina) - $qtd_result;
 
-                   $query_contratos = "SELECT * FROM contratos LIMIT $start, $qtd_result";    
+                    
+
+                   $query_contratos = "SELECT * FROM contratos";    
                    $exec_contratos = mysqli_query($conn, $query_contratos);
                    $reg_contratos = mysqli_num_rows($exec_contratos);
                    while($row_contrato = mysqli_fetch_assoc($exec_contratos)){
@@ -421,12 +419,6 @@
                       ";
                     }
                    }
-                        $result_pg = "SELECT COUNT(id) AS num_result FROM servicos";
-                        $resultado_pg = mysqli_query($conn, $result_pg);
-                        $row_pg = mysqli_fetch_assoc($resultado_pg);
-                        $quantidade_pg = ceil($row_pg['num_result'] / $qtd_result);
-                        $max_links = 1;
-
                   ?> 
                   </tbody>
                 </table>
@@ -436,30 +428,18 @@
                   }
                 ?>
               </div>
-              <?php    
-                echo "<div class='card-footer clearfix'>
-                <ul class='pagination pagination-sm m-0 float-right'>
-                  <li class='page-item'><a class='page-link' href='contratos.php?pagina=1'>&laquo;</a></li>";
-
-                  for($pag_ant = $pagina - $max_links; $pag_ant <= $pagina - 1; $pag_ant++){
-                    if($pag_ant >= 1){
-                        echo "<li class='page-item'><a class='page-link' href='contratos.php?pagina=$pag_ant'>$pag_ant</a></li>";
-                    }
-                }
-                echo"<li class='page-item'><a style='background-color: #E9ECEF;' class='page-link'>$pagina</a></li>";
-
-                for($pag_dep = $pagina + 1; $pag_dep <= $pagina + $max_links; $pag_dep++){
-                if($pag_dep <= $quantidade_pg){
-                    echo "<li class='page-item'><a class='page-link' href='contratos.php?pagina=$pag_dep'>$pag_dep</a></li>";
-                }
-                }
-                echo"
-                  <li class='page-item'><a class='page-link' href='contratos.php?pagina=$quantidade_pg'>&raquo;</a></li>
-                </ul>
-                </div>";
-              ?>
               <!-- /.card-body -->
+              <div class="card-footer clearfix">
+                <ul class="pagination pagination-sm m-0 float-right">
+                  <li class="page-item"><a class="page-link" href="#">&laquo;</a></li>
+                  <li class="page-item"><a class="page-link" href="#">1</a></li>
+                  <li class="page-item"><a class="page-link" href="#">2</a></li>
+                  <li class="page-item"><a class="page-link" href="#">3</a></li>
+                  <li class="page-item"><a class="page-link" href="#">&raquo;</a></li>
+                </ul>
+              </div>
             </div>
+
           </div>
         </div>
       </div>
