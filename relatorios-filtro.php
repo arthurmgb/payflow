cfb434ad3a861473c3d28be2117aac04a1574d9c
Arthur Oliveira <arthurpmgb@gmail.com>
2020-11-23 22:26:41 -0300

    updates

---
+++ relatorios-filtro.php
@@ -0,0 +1,459 @@
+<?php 
+  include_once("conexao.php");
+  session_start();
+  if(!empty($_SESSION['id'])){
+      
+  }
+  else{
+    header("Location: login.php");
+  }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>PayFlow | Relatórios</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!--Favicons-->
+  <link rel="apple-touch-icon" sizes="180x180" href="dist/favicon/">
+  <link rel="icon" type="image/png" sizes="32x32" href="dist/favicon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="dist/favicon/favicon-16x16.png">
+  <link rel="manifest" href="dist/favicon/site.webmanifest">
+  <link rel="mask-icon" href="dist/favicon/safari-pinned-tab.svg" color="#5bbad5">
+  <meta name="msapplication-TileColor" content="#da532c">
+  <meta name="theme-color" content="#ffffff">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Tempusdominus Bbootstrap 4 -->
+  <link rel="stylesheet" href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- JQVMap -->
+  <link rel="stylesheet" href="plugins/jqvmap/jqvmap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
+  <!-- summernote -->
+  <link rel="stylesheet" href="plugins/summernote/summernote-bs4.css">
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Fredoka+One&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="dist/css/style.css">
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+<?php 
+      require_once("info-perfil.php");
+?>
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+    </ul>
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item dropdown">
+        <a class="nav-link" data-toggle="dropdown" href="#">
+          <i class="fas fa-user-cog color-fas"></i>
+        </a>
+        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+          <span class="drop-name dropdown-item dropdown-header"><?= $user_name ?></span>
+          <div class="dropdown-divider"></div> 
+            <div class="user-panel">
+              <!-- Painel -->
+            <?php 
+            if((isset($user_foto) AND !empty($user_foto))){
+              echo"
+              <img src='usuarios/$user_id/$user_foto' class='img-circle elevation-2 my-4 mx-auto d-block user-edit' alt='User Image'>
+              ";
+            }else{
+              echo"
+              <img src='dist/img/avatar5.png' class='img-circle elevation-2 my-4 mx-auto d-block user-edit' alt='User Image'>
+              ";
+            }
+            ?>
+            <!-- Fim Painel -->
+            </div>
+          <div class="dropdown-divider"></div>
+          <a href="usuario.php" class="btn btn-info float-left my-2 mx-2" role="button"><i class="mg-button fas fa-user-edit"></i> Perfil</a>
+          <a href="sair.php" class="btn btn-danger float-right my-2 mx-2" role="button">Sair <i class="mg-button fas fa-sign-out-alt"></i></a>
+        </div>
+      </li>
+    </ul>
+  </nav>
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-light-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index.php" class="brand-link">
+      <img src="dist/img/favicon.png" alt="PayFlow" class="brand-margin brand-image rounded d-block">
+      <span class="brand-text d-block fredoka">PayFlow</span>
+    </a>
+
+    <!-- Info Perfil -->
+
+    <div class="sidebar">
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <?php 
+            if((isset($user_foto) AND !empty($user_foto))){
+              echo"
+              <img src='usuarios/$user_id/$user_foto' class='img-circle elevation-2' alt='User Image'>
+              ";
+            }else{
+              echo"
+              <img src='dist/img/avatar5.png' class='img-circle elevation-2' alt='User Image'>
+              ";
+            }
+          ?>
+          
+        </div>
+        <div class="info">
+          <a href="usuario.php" class="d-block"><?= $user_name ?></a>
+        </div>
+      </div>
+
+    <!-- Fim de Info Perfil -->
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item">
+            <a href="index.php" class="nav-link">
+              <i class="fas fa-chart-bar nav-icon"></i>
+              <p>
+                Painel Corporativo  
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="contratos.php" class="nav-link">
+              <i class="fas fa-file-signature nav-icon"></i>
+              <p>
+                Contratos
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="clientes.php" class="nav-link">
+              <i class="fas fa-users nav-icon"></i>
+              <p>
+                Clientes
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="mensalidades.php" class="nav-link">
+              <i class="far fa-money-bill-alt nav-icon"></i>
+              <p>
+                Mensalidades
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="servicos.php" class="nav-link">
+              <i class="fas fa-tasks nav-icon"></i>
+              <p>
+                Serviços
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="saldo.php" class="nav-link">
+              <i class="fas fa-cash-register nav-icon"></i>
+              <p>
+                Saldo
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="relatorios.php" class="nav-link active">
+              <i class="fas fa-file-invoice-dollar nav-icon"></i>
+              <p> 
+                Relatórios
+              </p>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0 text-dark">Relatórios</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Relatórios</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+    	<div class="card border-top">
+			<h4 class="m-2 text-dark ml-3">Filtrar Mensalidades</h4>
+				<form action="relatorios-filtro.php" method="GET" class="sec-pad">
+                <?php 
+                    $status = filter_input(INPUT_GET, 'situacao');
+                    $nomeServico = filter_input(INPUT_GET, 'servico');
+                    $vencimento = filter_input(INPUT_GET, 'vencimento');
+                    $fim_venc = filter_input(INPUT_GET, 'fim_venc');
+                    $pagamento = filter_input(INPUT_GET, 'pagamento');
+                    $fim_pag = filter_input(INPUT_GET, 'fim_pag');
+                ?>
+					<div class="form-row">
+						<div class="form-group col-md-6 mt-2">
+							<label class="font-weight-bold ml-2">Status</label>
+								<select name="situacao" class="form-control">
+                    <option value="pago">Pago</option>
+                    <option value="pendente">Pendente</option>
+                    <option value="anulada">Anulada</option>
+		    					</select>
+		    			</div>
+	    			<div class="form-group col-md-6 mt-2">
+    					<label class="font-weight-bold ml-2">Serviço</label>
+	    					<select name="servico" class="form-control">
+                <?php
+                $get_servicos = "SELECT * FROM servicos WHERE modo='Ativo'"; 
+                $exec_get = mysqli_query($conn, $get_servicos);         
+                ?>
+                <?php while($servico = mysqli_fetch_assoc($exec_get)): ?>
+	      					<option value="<?= $servico['id'] ?>"><?= $servico['servicos'] ?></option>
+                <?php endwhile ?>
+	    					</select>
+	    			</div>
+				</div> <!-- /. form-row -->
+
+				<div class="form-row">
+					<div class="form-group col-md-6 mt-2">
+						<label class="font-weight-bold ml-2" >Vencimento</label>
+						<input value="<?= $vencimento ?>" type="date" name="vencimento" class="form-control">
+					</div>
+					<div class="form-group col-md-6 mt-2">
+						<label class="font-weight-bold ml-2">Até</label>
+						<input value="<?= $fim_venc ?>" type="date" name="fim_venc" class="form-control">
+					</div>
+				</div>
+
+				<div class="form-row">
+					<div class="form-group col-md-6 mt-2">
+						<label class="font-weight-bold ml-2">Pagamento</label>
+						<input value="<?= $pagamento ?>" type="date" name="pagamento" class="form-control">
+					</div>
+					<div class="form-group col-md-6 mt-2">
+						<label class="font-weight-bold ml-2">Até</label>
+						<input value="<?= $fim_pag ?>" type="date" name="fim_pag" class="form-control">
+					</div>
+				</div>
+			
+			
+                <div class="row">
+                    <div class="col-9">
+                    <button type="submit" class="btn btn-success btn-lg btn-block"><i class="fas fa-search mr-2"></i>Buscar</button>
+                    </div>
+                    <div class="col-3">
+                    <a class="btn btn-primary btn-lg btn-block" href="relatorios.php"><i class="fas fa-undo-alt mr-2"></i>Limpar busca</a>
+                    </div>
+                </div>    		   
+      	        
+
+			</form>
+      	</div> <!-- /.card --> 
+
+      	<div class="card border-top">   		
+			<div class="row navbar">
+				<h4 class="text-dark ml-3">Resultados</h4>	
+			</div>
+
+			<div class="table-responsive">
+                <?php  
+
+                    if($status == 'pago'){
+                        if($vencimento AND $fim_venc){
+                            $filtro_mensal = "SELECT * FROM mensalidades WHERE status='$status' AND id_servico='$nomeServico' AND vencimento BETWEEN '$vencimento' AND '$fim_venc'";
+                            $exec_filtro = mysqli_query($conn, $filtro_mensal);
+                            $quantidade = mysqli_num_rows($exec_filtro);
+                        }elseif($pagamento AND $fim_pag){
+                            $filtro_mensal = "SELECT * FROM mensalidades WHERE status='$status' AND id_servico='$nomeServico' AND pagamento BETWEEN '$pagamento' AND '$fim_pag'";
+                            $exec_filtro = mysqli_query($conn, $filtro_mensal);
+                            $quantidade = mysqli_num_rows($exec_filtro);
+                        }
+                        else{
+                            $filtro_mensal = "SELECT * FROM mensalidades WHERE status='$status' AND id_servico='$nomeServico'";
+                            $exec_filtro = mysqli_query($conn, $filtro_mensal);
+                            $quantidade = mysqli_num_rows($exec_filtro);
+                        }
+                        
+                    }
+
+                    elseif($status == 'pendente'){
+                        if($vencimento AND $fim_venc){
+                        $filtro_mensal = "SELECT * FROM mensalidades WHERE status='$status' AND id_servico='$nomeServico' AND vencimento BETWEEN '$vencimento' AND '$fim_venc'";
+                        $exec_filtro = mysqli_query($conn, $filtro_mensal);
+                        $quantidade = mysqli_num_rows($exec_filtro);
+                        }else{
+                            $filtro_mensal = "SELECT * FROM mensalidades WHERE status='$status' AND id_servico='$nomeServico'";
+                            $exec_filtro = mysqli_query($conn, $filtro_mensal);
+                            $quantidade = mysqli_num_rows($exec_filtro);
+                        }
+                    }
+
+                    elseif($status == 'anulada'){
+                        if($vencimento AND $fim_venc){
+                        $filtro_mensal = "SELECT * FROM mensalidades WHERE status='$status' AND id_servico='$nomeServico' AND vencimento BETWEEN '$vencimento' AND '$fim_venc'";
+                        $exec_filtro = mysqli_query($conn, $filtro_mensal);
+                        $quantidade = mysqli_num_rows($exec_filtro);
+                        }else{
+                            $filtro_mensal = "SELECT * FROM mensalidades WHERE status='$status' AND id_servico='$nomeServico'";
+                            $exec_filtro = mysqli_query($conn, $filtro_mensal);
+                            $quantidade = mysqli_num_rows($exec_filtro);
+                        }
+                    }
+
+                ?>
+				<table class="table table-striped text-center">
+				  <thead>
+				    <tr>
+				      <th width="35%">Cliente</th>
+				      <th>Serviço</th>
+				      <th>Valor</th>
+				      <th>Status</th>
+				      <th>Vencimento</th>
+				      <th>Data de pagamento</th>
+				    </tr>
+				  </thead>
+
+				  <tbody>
+                    <?php 
+                    
+                    while($resultado = mysqli_fetch_assoc($exec_filtro)): 
+                    
+                        //BUSCANDO CLIENTE
+                        $get_cliente = "SELECT nome FROM clientes WHERE id='{$resultado['id_cliente']}'";
+                        $exec_cliente = mysqli_query($conn, $get_cliente);
+                        $name_cliente = mysqli_fetch_assoc($exec_cliente);
+
+                        //BUSCANDO SERVIÇO
+                        $get_servico = "SELECT servicos FROM servicos WHERE id='{$resultado['id_servico']}'";
+                        $exec_servico = mysqli_query($conn, $get_servico);
+                        $name_servico = mysqli_fetch_assoc($exec_servico);
+
+                        //FORMAT DATAS
+                        $format_venc = date('d/m/Y', strtotime($resultado['vencimento'])); 
+                        $format_pag = date('d/m/Y', strtotime($resultado['pagamento']));
+
+                        if($format_pag == '30/11/-0001'){
+                            $format_pag = '';
+                        }
+                        
+                    ?>
+
+				    <tr>
+				      <td><?= $name_cliente['nome'] ?></td>
+				      <td><?= $name_servico['servicos'] ?></td>
+				      <td style="color: green; font-weight: 600;">R$ <?= $resultado['valor'] ?></td>
+				      <td class="text-uppercase"><?= $resultado['status'] ?></td>
+				      <td style="color: red;"><?= $format_venc ?></td>
+				      <td style="color: green;"><?= $format_pag ?></td>
+				    </tr>
+
+                    <?php endwhile ?>
+
+                    <?php if($quantidade === 0): ?>
+                    <tr>
+                        <td>Nenhum registro encontrado.</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <?php endif ?>
+
+				  </tbody>
+				</table>
+
+			</div>
+
+      	</div>
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+  <footer class="main-footer">
+    <strong>Copyright &copy; <?= date('Y')?> <a href="index.php">PayFlow</a>.</strong>
+    Todos os direitos reservados.
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="plugins/jquery-ui/jquery-ui.min.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+  $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- ChartJS -->
+<script src="plugins/chart.js/Chart.min.js"></script>
+<!-- Sparkline -->
+<script src="plugins/sparklines/sparkline.js"></script>
+<!-- JQVMap -->
+<script src="plugins/jqvmap/jquery.vmap.min.js"></script>
+<script src="plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+<!-- jQuery Knob Chart -->
+<script src="plugins/jquery-knob/jquery.knob.min.js"></script>
+<!-- daterangepicker -->
+<script src="plugins/moment/moment.min.js"></script>
+<script src="plugins/daterangepicker/daterangepicker.js"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<!-- Summernote -->
+<script src="plugins/summernote/summernote-bs4.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="dist/js/pages/dashboard.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="dist/js/demo.js"></script>
+</body>
+</html>
